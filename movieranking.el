5faf04a0090482c6a0132e30cd9873d13a5e67ac
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2023-08-12 18:33:14 -0400

    Refactor: rater table maps a movie ID only to a rating

--- movieranking.el
+++ movieranking.el
@@ -49,44 +49,46 @@ Movies are indicated by unique ID numbers."
     (cl-delete-if (lambda (rater-id) (string-empty-p rater-id)) (cdr lists) :key #'car)
 
     (dolist (row (cdr lists) table)     ; skip the header row
-      (let* ((rater-id (car row))
-             (movie-id (cadr row))
-             (entry (gethash rater-id
-                             table
-                             (make-hash-table :test #'equal))))
-        (puthash movie-id (cddr row) entry)
-        (puthash rater-id entry table)))))
+      (seq-let (rater-id movie-id rating) row
+        (let ((entry (gethash rater-id
+                              table
+                              (make-hash-table :test #'equal))))
+          (puthash movie-id rating entry)
+          (puthash rater-id entry table))))))
 
 (defun compute-dot-product (rater-table rater-id-1 rater-id-2)
   "Compute the dot product of two rows in the given RATER-TABLE."
   (let ((row1 (gethash rater-id-1 rater-table))
         (row2 (gethash rater-id-2 rater-table))
         (dot-product 0))
-    (dohash (movie-id info-1 row1 dot-product)
+    (dohash (movie-id rating-1 row1 dot-product)
       (cl-flet ((normalize (rating)
                   (- (string-to-number rating) 5)))
-        (let ((rating-1 (car info-1)))
-          ;; Compute dot-product only when the two raters have a movie
-          ;; in common.
-          (when-let ((info-2 (gethash movie-id row2)))
-            (let ((rating-2 (car info-2)))
-              (cl-incf dot-product
-                       (* (normalize rating-1)
-                          (normalize rating-2))))))))))
+        (when-let ((rating-2 (gethash movie-id row2)))
+          (cl-incf dot-product
+                   (* (normalize rating-1)
+                      (normalize rating-2))))))))
 
 (defun compute-ratings-table (rater-table)
-  (let ((table (make-hash-table :test #'equal)))
-    (dohash (rater-id movie-table rater-table table)
-      (dohash (movie-id info movie-table)
+  (let ((ratings-table (make-hash-table :test #'equal)))
+    (dohash (rater-id movie-table rater-table ratings-table)
+      (dohash (movie-id rating movie-table)
         (let ((entry (gethash movie-id
-                              table
+                              ratings-table
                               (make-hash-table :test #'equal))))
-          (puthash rater-id info entry)
-          (puthash movie-id entry table))))))
+          (puthash rater-id rating entry)
+          (puthash movie-id entry ratings-table))))))
 
 (defun compute-coefficient-table (rater-table rater-id)
   (error "To be implemented."))
 
+(defun print-hash-table (hash-table)
+  "Print a hash table's key-value pairs."
+  (dohash (key value hash-table)
+    (message "%s %s" key value)))
+
 (let ((table (compute-rater-table "data/ratings_short.csv")))
+  (print-hash-table table)
   (compute-dot-product table "1" "2")
-  (compute-ratings-table table))
+  (let ((result (compute-ratings-table table)))
+    (print-hash-table result)))
